62e80150f1948c788f81ceea60fbe790076b08d3
Ibragim <72756511+Ibrakam@users.noreply.github.com>
2024-10-11 18:58:22 +0500

    sdf

--- user/users.py
+++ user/users.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from user.forms import RegisterForm, LoginForm
 
 user_bp = Blueprint("users", __name__, url_prefix="/user")
@@ -11,11 +11,28 @@ def main():
     return f"Добро пожаловать на наш сайт {reg_url + login_url}"
 
 
-@user_bp.route("/register")
-def root():
+user = []
+
+
+@user_bp.route("/register", methods=['GET', 'POST'])
+def root_register():
     form = RegisterForm()
+    print("register")
+    if request.method == "POST":
+        if form.validate_on_submit():
+            print("register")
+            name = form.name.data
+            email = form.email.data
+            user.append(name)
+            user.append(email)
+            print(user)
+            return "Регистрация прошла успешно"
 
     return render_template("register.html", form=form)
 
 
+@user_bp.route("/login")
+def root_login():
+    form = LoginForm()
 
+    return render_template("login.html", form=form)
